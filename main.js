d3f59c5124192a96cb4315d5585c659be3933908
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-02-09 13:29:55 +0545

    localstorage

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
+function setLocalStorageItem(todoArray) {
+    localStorage.setItem("todoArray", JSON.stringify(todoArray));
+}
+
+function getLocalStorageItem() {
+    const todoArray = localStorage.getItem("todoArray");
+    const parsedTodo = JSON.parse(todoArray);
+    return parsedTodo;
+}
+
+function mapTodoItem() {
+    const arrays = getLocalStorageItem();
+
+    console.log(arrays);
+
+    if (arrays) {
+        arrays.forEach((item) => {
+            const ulList = document.getElementById("ul-list");
+
+            //li item  <li class="todo"></li>
+            const li = document.createElement("li");
+            li.setAttribute("class", "todo");
+
+            const liElement = `
+            <div class="todo-item">
+                <div>
+                    <h4>${item.title}</h4>
+                    <p>${item.description} </p>
+                </div>
+                <div>
+                    <button class="button">Complete</button>
+                    <button class="button bg-red">Delete</button>
+                </div>
+            </div>
+            
+            `;
+
+            li.innerHTML = liElement;
+            ulList.appendChild(li);
+        });
+    }
+}
+mapTodoItem();
+
+getLocalStorageItem();
+
 function submitForm(event) {
     event.preventDefault();
 
@@ -19,61 +65,39 @@ function submitForm(event) {
     const li = document.createElement("li");
     li.setAttribute("class", "todo");
 
-    ///div inside li <div class="todo-item"></div>
-    const mainWrapper = document.createElement("div");
-    mainWrapper.setAttribute("class", "todo-item");
+    const liElement = `
+        <div class="todo-item">
+            <div>
+                <h4>${title}</h4>
+                <p>${description} </p>
+            </div>
+            <div>
+                <button class="button">Complete</button>
+                <button class="button bg-red">Delete</button>
+            </div>
+        </div>
 
-    //div that wraps title and description
-    /*
-       <div>
-          <h4>Hello</h4>
-          <p>Description </p>
-      </div>
-      */
-    const titleWrapper = document.createElement("div");
+    `;
 
-    const h4 = document.createElement("h4");
-    h4.innerHTML = todoItem.title;
-
-    const p = document.createElement("p");
-    p.innerHTML = todoItem.description;
-
-    titleWrapper.appendChild(h4);
-    titleWrapper.appendChild(p);
-
-    /**
-       *  <div>
-              <button class="button">Complete</button>
-              <button class="button bg-red">Delete</button>
-          </div>
-       */
-    const buttonWrapper = document.createElement("div");
-
-    const completeButton = document.createElement("button");
-    completeButton.setAttribute("class", "button");
-    completeButton.innerHTML = "Complete";
-
-    const deleteButton = document.createElement("button");
-    deleteButton.setAttribute("class", "button bg-red");
-    deleteButton.innerHTML = "Delete";
-
-    buttonWrapper.appendChild(completeButton);
-    buttonWrapper.appendChild(deleteButton);
-
-    //add everting to div
-    mainWrapper.append(titleWrapper);
-    mainWrapper.append(buttonWrapper);
-
-    //add everything to li
-    li.appendChild(mainWrapper);
-
-    console.log(li);
-
+    li.innerHTML = liElement;
     ulList.appendChild(li);
 
-
     //clear the form for next todo input
-    form["title"].value = ""
-    form["description"].value = ""
-    form["priority"].value = ""
+    form["title"].value = "";
+    form["description"].value = "";
+    form["priority"].value = "";
+
+
+    //save data to storage
+    const arrays = getLocalStorageItem();
+
+    let newArray = [];
+
+    if (arrays) {
+        newArray = [...arrays, todoItem];
+    } else {
+        newArray = [todoItem];
+    }
+
+    setLocalStorageItem(newArray);
 }
